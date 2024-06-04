01d6023ee2d0f430b8eee13e6a56da87da5673b7
shashikant010 <shashikantyadav0503@gmail.com>
2024-06-04 14:54:35 +0530

    made a crousal half and a reveal text animation

---
+++ crousalInReact/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState,useCallback } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count,setCount]=useState(0)
+  const slides = document.querySelectorAll(".slide")
+
+  slides.forEach((slide,index)=>{
+    slide.style.left=`${index*100}%`;
+  })
+
+
+  useEffect(()=>{ 
+    
+  },[count])
+    const slideImage=()=>{
+      slides.forEach((slide)=>{
+        if(count>=slides.length-1) setCount(0)
+        slide.style.transform=`translateX(-${count*100}%)`
+      })
+      setCount(count+1)
+      console.log(count)
+      
+    }
+
+    slides.forEach(async(slide)=>{
+      const slidingImage = await setTimeout(slideImage,3000)
+    })
+
+  return (
+    <>
+    <main>
+        <img src="https://picsum.photos/id/237/1000/500" className="slide" />
+        <img src="https://picsum.photos/id/238/1000/500" className="slide" />
+        <img src="https://picsum.photos/id/239/1000/500" className="slide" />
+        <img src="https://picsum.photos/id/243/1000/500" className="slide" />
+    </main>
+    <div className="nav">
+     
+    </div>
+    </>
+  )
+}
+
+export default App
